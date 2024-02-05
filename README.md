3feb65e00c2cfb7cc765f3a34086ed5e3ad2a493
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-02-05 13:59:16 -0300

    alterações no método de teste

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# data-driven-cy
+# Testes com Data Driven 
+### Modelo com cypress usando Sauce Demo 
+
+Sauce Labs Demo: https://www.saucedemo.com/
+
+## Funcionalidade: Login
+# Cenários:
+    -1: Deve fazer login com sucesso - Multiplos usuários ( usando Data-Driven-testing )
+    -2: Deve fazer login com sucesso - Forma comum ( apenas para comparar )
+
+## Clonando e executando em sua máquina
+
+### Pré-requisito:
+
+-Node.js - Você encontra em: https://nodejs.org/en/
+
+-Visual Studio Code ou qualquer editor de texto - você encontra em: https://code.visualstudio.com/download
+
+-Git: você encontra em: https://git-scm.com/downloads
+
+Via terminal, rode os seguintes comandos:
+```  
+git clone https://github.com/fabioaraujoqa/data-driven-cy.git
+```
+```
+cd data-driven-cy
+```
+
+#### Para instalar as dependencias:
+```
+npm install 
+```
+
+#### Para executar em moodo Headlesss via console:
+```
+npx cypress run
+```
+
+#### Para executar via Dashboard:
+```
+npx cypress open 
+```
+Após abrir o dasboard, escolha "E2E Testing" e um navegador para excutar.
+
+
+### Bibliotecas de apoio:
+-Cypress: Framework de automação: https://cypress.io/
+
+
+
+### Boa diversão ;) 
+Fábio Araújo
+https://www.linkedin.com/in/fabio10/
+
+
+
+
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,30 @@
 describe('Login - Data-Driven Tests on Sauce Demo', () => {
   beforeEach(() => {
-    cy.visit('/'); 
+    cy.visit('/'); //Visita o site saucelabs. Config em cypress.config.js
   });
 
-  it('Deve fazer login com multiplos usuários', () => {
-    cy.fixture('loginData').then((users) => {
-      users.forEach((user) => {
-        cy.get('[data-test="username"]').clear().type(user.username);
-        cy.get('[data-test="password"]').clear().type(user.password);
-        cy.get('[data-test="login-button"]').click();
+  it('Deve fazer login com sucesso - Multiplos usuários', () => {
+    cy.fixture('loginData').then((users) => { // Fixture carrega os usuários de uma lista
+      users.forEach((user) => { // forEach percorrer todo os itens de uma lista Ex. 'loginData.json'
+        cy.get('[data-test="username"]').clear().type(user.username) //Preenche login de um arquivo
+        cy.get('[data-test="password"]').clear().type(user.password) //Preenche senha de um arquivo
+        cy.get('[data-test="login-button"]').click(); //Clica no botão login
+        cy.get('.inventory_list').should('be.visible') // Faz a validação do login
 
-        if (user.username !== 'locked_out_user') {
-          cy.get('.inventory_list').should('be.visible'); 
-          cy.get('#react-burger-menu-btn').click(); 
-          cy.get('#logout_sidebar_link').click(); 
-        } else {
-          cy.get('[data-test="error"]').should('contain', 'Sorry, this user has been locked out.'); 
-          cy.reload(); 
-        }
+        //Passos para sair do login, pois o sistema não permite reset da aplicação
+        cy.get('#react-burger-menu-btn').click() // Abre o meno lateral
+        cy.get('#logout_sidebar_link').click()  //Clica em Logout
+        cy.reload(); //Recarrega a página
+
       });
     });
+  });
+
+  it('Deve fazer login com sucesso - Forma comum', () => {
+    // Teste afim de comparar a forma simples com a de multiplos usuários - Data Driven
+    cy.get('[data-test="username"]').clear().type('standard_user')
+    cy.get('[data-test="password"]').clear().type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('.inventory_list').should('be.visible')
   });
 });
